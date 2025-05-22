58b7f99b15c34acd0544bbb8cb85f658b63b40e6
Vuk7912 <Vuk7912@users.noreply.github.com>
2025-05-22 22:54:51 +0000

    Update Prisma setup with more explicit client management

--- prisma/setup.ts
+++ prisma/setup.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient()
-
 export async function setupTests() {
+  const prisma = new PrismaClient()
   await prisma.$connect()
+  return prisma
 }
 
-export async function teardownTests() {
+export async function teardownTests(prisma: PrismaClient) {
   await prisma.$disconnect()
 }
